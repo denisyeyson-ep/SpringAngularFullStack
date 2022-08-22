08f1efea8f539a582f8ccd50517850224ac17038
denisyeyson-ep <denisyeyson@outlook.com>
2022-08-22 12:37:32 -0500

    Primer commit

---
+++ src/app/clientes/clientes.json.ts
@@ -0,0 +1,9 @@
+import { Cliente } from "./cliente";
+
+export const CLIENTES: Cliente[] = [
+  { id: 1, nombre: 'Denis', apellido: 'Espinoza', email: 'denisyeyson@gmail.com', createAt: '26/02/1997' },
+  { id: 2, nombre: 'Julia', apellido: 'Ponciano', email: 'leandra@gmail.com', createAt: '27/02/1997' },
+  { id: 3, nombre: 'Eliseo', apellido: 'Espinoza', email: 'leandra@gmail.com', createAt: '27/02/1997' },
+  { id: 4, nombre: 'Griela', apellido: 'Espinoza', email: 'leandra@gmail.com', createAt: '27/02/1997' },
+  { id: 5, nombre: 'Leonard', apellido: 'Espinoza', email: 'leandra@gmail.com', createAt: '27/02/1997' }
+];
